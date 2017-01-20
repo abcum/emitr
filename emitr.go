b25121db3ef37ea2facbf667449e0fa7dd752abb
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-01-20 00:44:32 +0000

    Make library safe for concurrent access

--- emitr.go
+++ emitr.go
@@ -14,8 +14,11 @@
 
 package emitr
 
+import "sync"
+
 // Emitter represents an event emitter.
 type Emitter struct {
+	lock sync.Mutex
 	once map[string][]func()
 	when map[string][]func()
 }
@@ -23,25 +26,40 @@ type Emitter struct {
 // Once adds a callback which is called only once when an
 // event is emitted, and then removed immediately after.
 func (e *Emitter) Once(name string, fn func()) {
+
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
 	if e.once == nil {
 		e.once = make(map[string][]func())
 	}
+
 	e.once[name] = append(e.once[name], fn)
+
 }
 
 // When adds a callback which is called each and every
 // time that an event is emitted.
 func (e *Emitter) When(name string, fn func()) {
+
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
 	if e.when == nil {
 		e.when = make(map[string][]func())
 	}
+
 	e.when[name] = append(e.when[name], fn)
+
 }
 
 // Emit runs callbacks for the specified event name,
 // and ensures that one-off events, are removed when
 // the event has finished emitting.
 func (e *Emitter) Emit(name string) {
+
+	e.lock.Lock()
+	defer e.lock.Unlock()
 
 	if _, ok := e.when[name]; ok {
 		for i := len(e.when[name]) - 1; i >= 0; i-- {
